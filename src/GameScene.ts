5160286cfb86ece16f48ffff1f9af912edc82526
Vladisl0ve <vladislav.lapko@gmail.com>
2024-08-26 21:57:50 +0200

    Fixed game field borders

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -10,35 +10,46 @@ export class GameScene extends Container implements IScene {
     constructor() {
         super();
 
-        this.road = new TilingSprite(Texture.from("roadPng"), Manager.height, Manager.width)
-        this.road.anchor.set(0.5);
-        this.road.x = Manager.width / 2;
-        this.road.y = Manager.height / 4;
+        this.road = new TilingSprite(Texture.from("roadPng"), 1170, 2532)
+
+        this.road.scale.set(0.3678)
+
         this.addChild(this.road);
+        console.log(this.road)
         
         this.vehicle = Sprite.from("carPng");
-        this.vehicle.anchor.set(0);
+        this.vehicle.anchor.set(0.5);
+        this.vehicle.scale.set(0.5);
         this.vehicle.x = Manager.width / 2;
-        this.vehicle.y = Manager.height / 2;
-        this.addChild(this.vehicle);
+        this.vehicle.y = Manager.height;
+        this.road.addChild(this.vehicle);
         console.log(Manager.height + ": H| W: " + Manager.width)
+        console.log(this.vehicle.parent.height + ": H| W: " + this.vehicle.parent.width)
         console.log(this.vehicle.height + ": H| W: " + this.vehicle.width)
+        console.log(this.road);
+        console.log(this.vehicle.parent);
+
+        console.log()
 
         this.roadVelocity = 5;
 
         window.addEventListener('keydown', (event) => {
             switch (event.code) {
                 case 'ArrowUp':
-                    this.vehicle.y = Math.max(this.vehicle.height / 2, this.vehicle.y - 10); // Move vehicle up
+                    this.vehicle.y = Math.max(this.vehicle.height / 1.5, this.vehicle.y - 30); // Move vehicle up
+                    console.log(this.vehicle.x + " :x; y: " + this.vehicle.y);
                     break;
                 case 'ArrowDown':
-                    this.vehicle.y = Math.min(Manager.height - this.vehicle.height / 2, this.vehicle.y + 10); // Move vehicle down
+                    this.vehicle.y = Math.min(this.vehicle.parent.height - this.vehicle.height / 1.5, this.vehicle.y + 30); // Move vehicle down
+                    console.log(this.vehicle.x + " :x; y: " + this.vehicle.y);
                     break;
                 case 'ArrowLeft':
-                    this.vehicle.x = Math.max(this.vehicle.width / 2, this.vehicle.x - 10); // Move vehicle left
+                    this.vehicle.x = Math.max(this.vehicle.width / 1.5, this.vehicle.x - 30); // Move vehicle left
+                    console.log(this.vehicle.x + " :x; y: " + this.vehicle.y);
                     break;
                 case 'ArrowRight':
-                    this.vehicle.x = Math.min(Manager.width - this.vehicle.width / 2, this.vehicle.x + 10); // Move vehicle right
+                    this.vehicle.x = Math.min(this.vehicle.parent.width - this.vehicle.width / 1.5, this.vehicle.x + 30); // Move vehicle right
+                    console.log(this.vehicle.x + " :x; y: " + this.vehicle.y);
                     break;
             }
         });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Manager } from './Manager';
 import { LoaderScene } from './scenes/LoaderScene';
 
-Manager.initialize(1920, 1080, 0x6495ed);
+Manager.initialize(430, 932, 0x6495ed);
 
 // We no longer need to tell the scene the size because we can ask Manager!
 const loady: LoaderScene = new LoaderScene();
